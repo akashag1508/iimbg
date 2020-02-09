bec03c60b17c7b4f4fe47665a35c4cebe4edf162
akashag1508 <60791794+akashag1508@users.noreply.github.com>
2020-02-09 19:27:31 +0530

    j

---
+++ clustering.R
@@ -0,0 +1,16 @@
+#clustering
+
+set.seed(1234)
+subject1 = trunc(rnorm(30, mean = 60, sd = 15))
+range(subject1)
+subject1
+marks = data.frame(subject1)
+head(marks)
+k2 = kmeans(marks, centers = 2)
+k2
+k2$size
+length(marks[k2$cluster==1, ])
+marks[k2$cluster==2,]
+marks[k2$cluster==1,]
+
+mean()
